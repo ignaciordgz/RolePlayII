f21154a45d09fa4a72ed562573f29fef03873371
Nicolas Gonzalez <nicolasgonzalezgutierrez2017@gmail.com>
2023-09-24 23:09:36 -0300

    Interfaz

--- src/Program/Program.cs
+++ src/Program/Program.cs
@@ -1,7 +1,6 @@
 ﻿using System.Collections.Generic;
 using System;
 using Library;
-using System.Runtime.CompilerServices;
 
 namespace Program
 {
@@ -9,65 +8,67 @@ namespace Program
     {
         static void Main(string[] args)
         {
-            Caballero Gervacio = new Caballero("Gervacio");
+            ICharacter Gervacio = new Caballero("Gervacio");
+            
             Item Espada = new Item("Espada", 20, 3);
             Item Armadura = new Item("Armadura", 0, 30);
             
-            Gervacio.funda.AgregarItem(Espada);
-            Gervacio.funda.AgregarItem(Armadura);
+            ((Caballero)Gervacio).funda.AgregarItem(Espada);
+            
+            Gervacio.PrintInfo();
+            ((Caballero)Gervacio).funda.AgregarItem(Armadura);
             Combate CaballeroAD = new Combate();
             
-            int AtaqueCaballero = CaballeroAD.CalcularAtaqueTotal(Gervacio.funda.ListaItems);
-            int DefensaCaballero = CaballeroAD.CalcularDefensaTotal(Gervacio.funda.ListaItems);
+            int AtaqueCaballero = CaballeroAD.CalcularAtaqueTotal(((Caballero)Gervacio).funda.ListaItems);
+            int DefensaCaballero = CaballeroAD.CalcularDefensaTotal(((Caballero)Gervacio).funda.ListaItems);
             
-            Console.WriteLine($"Nombre del Caballero : {Gervacio.Name}");
+            Console.WriteLine($"Nombre del Caballero : {((Caballero)Gervacio).Name}");
             Console.WriteLine($"Ataque Total : {AtaqueCaballero}");
             Console.WriteLine($"Defensa Total : {DefensaCaballero}");
             Console.WriteLine("------------------");
-              
-
-            Enano Thorin = new Enano("Thorin");
+            {
+            ICharacter Thorin = new Enano("Thorin");
             Item hacha = new Item("hacha", 15, 2);
             Item escudo = new Item("escudo", 0, 25);
 
-            Thorin.mochila.AgregarItem(hacha);
-            Thorin.mochila.AgregarItem(escudo);
+            ((Enano)Thorin).mochila.AgregarItem(hacha);
+            ((Enano)Thorin).mochila.AgregarItem(escudo);
 
             Combate EnanoAD= new Combate();
-            int AtaqueEnano = EnanoAD.CalcularAtaqueTotal(Thorin.mochila.ListaItems);
-            int DefensaEnano = EnanoAD.CalcularDefensaTotal(Thorin.mochila.ListaItems);
+            int AtaqueEnano = EnanoAD.CalcularAtaqueTotal(((Enano)Thorin).mochila.ListaItems);
+            int DefensaEnano = EnanoAD.CalcularDefensaTotal(((Enano)Thorin).mochila.ListaItems);
 
-            Console.WriteLine($"Nombre del Enano : {Thorin.Name}");
+            Console.WriteLine($"Nombre del Enano : {((Enano)Thorin).Name}");
             Console.WriteLine($"Ataque Total : {AtaqueEnano}");
             Console.WriteLine($"Defensa Total : {DefensaEnano}");
             Console.WriteLine("------------------");      
     
     
-            Elfo Lego = new Elfo("Lego");
+            ICharacter Lego = new Elfo("Lego");
             Item Arco = new Item("Arco", 25, 0);
             Item Traje = new Item("Traje", 0, 25);
             
-            Lego.Bolsa.AgregarItem(Arco);
-            Lego.Bolsa.AgregarItem(Traje);
+            ((Elfo)Lego).Bolsa.AgregarItem(Arco);
+            ((Elfo)Lego).Bolsa.AgregarItem(Traje);
             Combate ElfoAD = new Combate();
             
-            int AtaqueElfo = ElfoAD.CalcularAtaqueTotal(Lego.Bolsa.ListaItems);
-            int DefensaElfo = ElfoAD.CalcularDefensaTotal(Lego.Bolsa.ListaItems);
+            int AtaqueElfo = ElfoAD.CalcularAtaqueTotal(((Elfo)Lego).Bolsa.ListaItems);
+            int DefensaElfo = ElfoAD.CalcularDefensaTotal(((Elfo)Lego).Bolsa.ListaItems);
             
-            Console.WriteLine($"Nombre del Elfo : {Lego.Name}");
+            Console.WriteLine($"Nombre del Elfo : {((Elfo)Lego).Name}");
             Console.WriteLine($"Ataque Total : {AtaqueElfo}");
             Console.WriteLine($"Defensa Total : {DefensaElfo}");
             Console.WriteLine("------------------");
     
              
-            Mago Rasagast = new Mago("Rasagast");   
+            ICharacter Rasagast = new Mago("Rasagast");   
             Item BastonMagico = new Item("Baston Magico",25,2);  
             
             List<Hechizos> Hechizos = new List<Hechizos>{new Hechizos("Bola de fuego", 25),new Hechizos("Incinerar", 30),new Hechizos("Congelar", 5)};
-            Rasagast.AgregarHechizos(Hechizos);
+            ((Mago)Rasagast).AgregarHechizos(Hechizos);
             
             Inventario SombreroMagico = new Inventario();
-            Rasagast.SombreroMagico.AgregarItem(BastonMagico);
+            ((Mago)Rasagast).SombreroMagico.AgregarItem(BastonMagico);
     
             int DañoAcumuladoHechizos = 0;
             foreach(var i in Hechizos)
@@ -80,28 +81,20 @@ namespace Program
             SombreroMagico.AgregarItem(LibroHechizos);
 
             Combate MagoAD = new Combate ();
-            int DefensaMago = MagoAD.CalcularDefensaTotal(Rasagast.SombreroMagico.ListaItems);
-            int AtaqueMago = MagoAD.CalcularAtaqueTotal(Rasagast.SombreroMagico.ListaItems);
+            int DefensaMago = MagoAD.CalcularDefensaTotal(((Mago)Rasagast).SombreroMagico.ListaItems);
+            int AtaqueMago = MagoAD.CalcularAtaqueTotal(((Mago)Rasagast).SombreroMagico.ListaItems);
 
-            Console.WriteLine($"Nombre del Mago : {Rasagast.Name}");
+            Console.WriteLine($"Nombre del Mago : {((Mago)Rasagast).Name}");
             Console.WriteLine($"Ataque Total : {DañoTotalMago}");
             Console.WriteLine($"Defensa Total : {DefensaMago}");
             Console.WriteLine("------------------");
 
-            Console.WriteLine(CaballeroAD.PeleaCaballeroEnano(Gervacio,Thorin));
-            Console.WriteLine(MagoAD.PeleaMagoElfo(Rasagast,Lego));
+            Console.WriteLine(CaballeroAD.PeleaCaballeroEnano((Caballero)Gervacio,(Enano)Thorin));
+            Console.WriteLine(MagoAD.PeleaMagoElfo((Mago)Rasagast,(Elfo)Lego));
             Console.WriteLine("------------------");
-            Console.WriteLine(Combate.Curar(Rasagast,Lego,Thorin,Gervacio));
+            Console.WriteLine(Combate.Curar((Mago)Rasagast,(Elfo)Lego,(Enano)Thorin,(Caballero)Gervacio));
 
-            ICharacter Gervacio2 = new Caballero("GERVACIO2");
-            
-            Item Espada1 = new Item("Espada", 20, 3);
-            Item Armadura1 = new Item("Armadura", 0, 30);
-            
-            ((Caballero)Gervacio2).funda.AgregarItem(Espada1);
-            ((Caballero)Gervacio2).funda.AgregarItem(Armadura1);
-            
-            Gervacio2.PrintInfo();
         }
         }
     }
+}
